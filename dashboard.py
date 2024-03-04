a1903df44d9943935f847a3e02061099e568b54a
putririgita14 <162147689+putririgita14@users.noreply.github.com>
2024-03-04 22:31:35 +0700

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,156 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+sns.set(style='dark')
+
+def get_total_count_by_hour_df(hour_df):
+    hour_count_df = hour_df.groupby(by="hours").agg({"count_cr": ["sum"]})
+    hour_count_df.columns = ['total_count']
+    return hour_count_df
+
+def count_by_day_df(day_df):
+    day_df_count_2011 = day_df.query('dteday >= "2011-01-01" and dteday < "2012-12-31"')
+    return day_df_count_2011
+
+def total_registered_df(day_df):
+    reg_df = day_df.groupby(by="dteday").agg({
+        "registered": "sum"
+    })
+    reg_df = reg_df.reset_index()
+    reg_df.rename(columns={"registered": "register_sum"}, inplace=True)
+    return reg_df
+
+def total_casual_df(day_df):
+    cas_df = day_df.groupby(by="dteday").agg({
+        "casual": ["sum"]
+    })
+    cas_df = cas_df.reset_index()
+    cas_df.rename(columns={"casual": "casual_sum"}, inplace=True)
+    return cas_df
+
+def sum_order(hour_df):
+    sum_order_items_df = hour_df.groupby("hours").count_cr.sum().sort_values(ascending=False).reset_index()
+    return sum_order_items_df
+
+def macem_season(day_df):
+    season_df = day_df.groupby(by="season").count_cr.sum().reset_index()
+    return season_df
+
+# Membaca data
+days_df = pd.read_csv("https://raw.githubusercontent.com/putririgita14/dicoding/main/day.csv")
+hours_df = pd.read_csv("https://raw.githubusercontent.com/putririgita14/dicoding/main/hour.csv")
+
+days_df.rename(columns={'yr':'year','mnth':'month','weekday':'one_of_week', 'weathersit':'weather_situation', 'windspeed':'wind_speed','cnt':'count_cr','hum':'humidity'},inplace=True)
+hours_df.rename(columns={'yr':'year','hr':'hours','mnth':'month','weekday':'one_of_week', 'weathersit':'weather_situation','windspeed':'wind_speed','cnt':'count_cr','hum':'humidity'},inplace=True)
+
+# Mengatur tampilan DataFrame
+datetime_columns = ["dteday"]
+for column in datetime_columns:
+    days_df[column] = pd.to_datetime(days_df[column])
+    hours_df[column] = pd.to_datetime(hours_df[column])
+
+min_date_days = days_df["dteday"].min()
+max_date_days = days_df["dteday"].max()
+
+min_date_hour = hours_df["dteday"].min()
+max_date_hour = hours_df["dteday"].max()
+
+# Inisialisasi aplikasi Streamlit
+with st.sidebar:
+    st.image("https://storage.googleapis.com/gweb-uniblog-publish-prod/original_images/image1_hH9B4gs.jpg")
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',
+        min_value=min_date_days,
+        max_value=max_date_days,
+        value=[min_date_days, max_date_days]
+    )
+
+main_df_days = days_df[(days_df["dteday"] >= str(start_date)) & (days_df["dteday"] <= str(end_date))]
+main_df_hour = hours_df[(hours_df["dteday"] >= str(start_date)) & (hours_df["dteday"] <= str(end_date))]
+
+hour_count_df = get_total_count_by_hour_df(main_df_hour)
+day_df_count_2011 = count_by_day_df(main_df_days)
+reg_df = total_registered_df(main_df_days)
+cas_df = total_casual_df(main_df_days)
+sum_order_items_df = sum_order(main_df_hour)
+season_df = macem_season(main_df_hour)
+
+# Melengkapi Dashboard dengan Berbagai Visualisasi Data
+st.header('Bike Sharing :sparkles:')
+st.subheader('Daily Sharing')
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    total_orders = day_df_count_2011.count_cr.sum()
+    st.metric("Total Sharing Bike", value=total_orders)
+
+with col2:
+    total_sum = reg_df.register_sum.sum()
+    st.metric("Total Registered", value=total_sum)
+
+with col3:
+    total_sum = cas_df.casual_sum.sum()
+    st.metric("Total Casual", value=total_sum)
+
+st.subheader("Rata - Rata Penyewaan Sepeda berdasarkan Kondisi Cuaca")
+
+avg_weather = days_df.groupby('weather_situation')['count_cr'].mean().reset_index().sort_values("count_cr")
+
+fig, ax = plt.subplots(figsize=(10, 6))
+
+sns.barplot(x='count_cr', y='weather_situation', data=avg_weather, palette=["#9B4444", "#9B4444", "#9B4444", "#9B4444"], ax=ax)
+
+ax.set_title('Rata - Rata Penyewaan Sepeda berdasarkan Kondisi Cuaca')
+ax.set_xlabel('Rata - Rata Penyewaan')
+ax.set_ylabel('Kondisi Cuaca')
+ax.tick_params(axis='x', rotation=45)
+
+st.pyplot(fig)
+
+st.subheader("Jam dengan Penyewa Sepeda Paling Banyak dan Paling Sedikit ")
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(35, 15))
+
+sns.barplot(x="hours", y="count_cr", data=sum_order_items_df.head(5), palette=["#D3D3D3", "#D3D3D3", "#90CAF9", "#D3D3D3", "#D3D3D3"], ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("Hours (PM)", fontsize=30)
+ax[0].set_title("Jam dengan banyak penyewa sepeda", loc="center", fontsize=30)
+ax[0].tick_params(axis='y', labelsize=35)
+ax[0].tick_params(axis='x', labelsize=30)
+
+sns.barplot(x="hours", y="count_cr", data=sum_order_items_df.sort_values(by="hours", ascending=True).head(5), palette=["#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3","#90CAF9"], ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("Hours (AM)", fontsize=30)
+ax[1].set_title("Jam dengan sedikit penyewa sepeda", loc="center", fontsize=30)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].tick_params(axis='y', labelsize=35)
+ax[1].tick_params(axis='x', labelsize=30)
+
+st.pyplot(fig)
+st.subheader("Musim yang paling sedikit disewa")
+
+# Use a different color palette or specify colors for each season
+colors = ["#E63946", "#F1FAEE", "#A8DADC", "#457B9D"]
+
+# membuat subplot dengan 1 baris dan 1 kolom, dengan ukuran (20, 10)
+fig, ax = plt.subplots(figsize=(20, 10))
+
+# Buat barplot untuk y="count_cr" dan x="season", menggunakan data=day_df
+sns.barplot(
+        y="count_cr", 
+        x="season",
+        data=days_df.sort_values(by="season", ascending=False),
+        palette=colors,
+        ax=ax
+    )
+# mengatur judul, label y dan x, serta tick params untuk subplot tersebut
+ax.set_title("Penyewaan Sepeda Sepanjang Musim", loc="center", fontsize=40)
+ax.set_ylabel(None)
+ax.set_xlabel(None)
+ax.tick_params(axis='x', labelsize=30)
+ax.tick_params(axis='y', labelsize=30)
+
+st.pyplot(fig)
